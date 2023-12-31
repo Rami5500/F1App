5980169be39d5c995ecf210296d8774c25acdac9
Rami5500 <Ramih5500@outlook.com>
2023-12-31 21:56:38 +0000

    Working_without_delete_feature

--- racing/app/src/main/java/uk/ac/aston/cs3mdd/racingapp/ui/home/HomeFragment.java
+++ racing/app/src/main/java/uk/ac/aston/cs3mdd/racingapp/ui/home/HomeFragment.java
@@ -15,6 +15,17 @@ import uk.ac.aston.cs3mdd.racingapp.databinding.FragmentHomeBinding;
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+<<<<<<< Updated upstream
+=======
+    private TextView textFavouriteDrivers;
+    private TextView textFavouriteTracks;
+    private TextView textVisitedTracks;
+
+    FavoritesFragment favoritesFragment = new FavoritesFragment();
+    private List<String> favoriteDrivers = new ArrayList<>();
+    private List<String> favoriteTracks = new ArrayList<>();
+    private List<String> visitedTracks = new ArrayList<>();
+>>>>>>> Stashed changes
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,11 +35,144 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+<<<<<<< Updated upstream
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
+=======
+        textFavouriteDrivers = root.findViewById(R.id.textFavouriteDrivers);
+        textFavouriteTracks = root.findViewById(R.id.textFavouriteTracks);
+        textVisitedTracks = root.findViewById(R.id.textVisitedTracks);
+
+        // Load and display favorites when the fragment is created
+        loadAndDisplayFavorites();
+
+
+        return root;
+    }
+
+    private void loadAndDisplayFavorites() {
+        String data = FileUtils.readFromFile(requireContext(), "favourites.json");
+
+        formatListWithDeleteButton(textFavouriteDrivers, favoriteDrivers, "deleteFavoriteDriver");
+        formatListWithDeleteButton(textFavouriteTracks, favoriteTracks, "deleteFavoriteTrack");
+        formatListWithDeleteButton(textVisitedTracks, visitedTracks, "deleteVisitedTrack");
+
+        try {
+            JSONObject favoritesObject = new JSONObject(data);
+
+            JSONArray favoriteTracksArray = favoritesObject.getJSONArray("favouriteTracks");
+            JSONArray favoriteDriversArray = favoritesObject.getJSONArray("favouriteDrivers");
+            JSONArray visitedTracksArray = favoritesObject.getJSONArray("visitedTracks");
+
+            // Clear the existing lists
+            favoriteDrivers.clear();
+            favoriteTracks.clear();
+            visitedTracks.clear();
+
+            // Populate the class-level lists
+            for (int i = 0; i < favoriteDriversArray.length(); i++) {
+                String driver = favoriteDriversArray.getString(i);
+                if (!favoriteDrivers.contains(driver)) {
+                    favoriteDrivers.add(driver);
+                }
+            }
+
+            for (int i = 0; i < favoriteTracksArray.length(); i++) {
+                String track = favoriteTracksArray.getString(i);
+                if (!favoriteTracks.contains(track)) {
+                    favoriteTracks.add(track);
+                }
+            }
+
+            for (int i = 0; i < visitedTracksArray.length(); i++) {
+                String visitedTrack = visitedTracksArray.getString(i);
+                if (!visitedTracks.contains(visitedTrack)) {
+                    visitedTracks.add(visitedTrack);
+                }
+            }
+
+            formatListWithDeleteButton(textFavouriteDrivers, favoriteDrivers, "deleteFavoriteDriver");
+            formatListWithDeleteButton(textFavouriteTracks, favoriteTracks, "deleteFavoriteTrack");
+            formatListWithDeleteButton(textVisitedTracks, visitedTracks, "deleteVisitedTrack");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void formatListWithDeleteButton(TextView textView, List<String> list, String deleteAction) {
+        SpannableStringBuilder builder = new SpannableStringBuilder();
+        for (String item : list) {
+            // Add item text
+            builder.append("- ").append(item).append(" ");
+
+            // Add delete button
+            ClickableSpan clickableSpan = new ClickableSpan() {
+                @Override
+                public void onClick(@NonNull View widget) {
+                    // Handle delete button click
+                    onDeleteButtonClick(deleteAction, item);
+                }
+            };
+            builder.setSpan(clickableSpan, builder.length() - item.length() - 2, builder.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+
+        // Set the text with the clickable spans
+        textView.setText(builder);
+
+        // Set the movement method for the TextView to make clickable spans work
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+
+    private void onDeleteButtonClick(String deleteAction, String item) {
+        switch (deleteAction) {
+            case "deleteFavoriteDriver":
+                deleteFavoriteDriver(item);
+                break;
+            case "deleteFavoriteTrack":
+                deleteFavoriteTrack(item);
+                break;
+            case "deleteVisitedTrack":
+                deleteVisitedTrack(item);
+                break;
+        }
+    }
+
+
+    private void deleteFavoriteDriver(String driver) {
+        Log.d("Delete", "Before deletion - favoriteDrivers: " + favoriteDrivers);
+
+        favoriteDrivers.remove(driver);
+
+        Log.d("Delete", "After deletion - favoriteDrivers: " + favoriteDrivers);
+
+        favoritesFragment.saveFavourites(favoriteDrivers, favoriteTracks, visitedTracks);
+    }
+
+    private void deleteFavoriteTrack(String track) {
+        favoriteTracks.remove(track);
+        favoritesFragment.saveFavourites(favoriteDrivers, favoriteTracks, visitedTracks);
+    }
+
+    private void deleteVisitedTrack(String track) {
+        visitedTracks.remove(track);
+        favoritesFragment.saveFavourites(favoriteDrivers, favoriteTracks, visitedTracks);
+    }
+
+
+    private String formatListAsString(List<String> list) {
+        StringBuilder builder = new StringBuilder();
+        for (String item : list) {
+            builder.append("- ").append(item).append("\n");
+        }
+        return builder.toString();
+    }
+
+>>>>>>> Stashed changes
     @Override
     public void onDestroyView() {
         super.onDestroyView();
